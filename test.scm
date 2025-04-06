1f9ad7058f37f456a5248076e8a39f51cbe1b2fc
gregoriB <brandon.gregori@gmail.com>
2025-04-06 11:25:48 -0400

    Update test file

--- test.scm
+++ test.scm
@@ -1,20 +1,29 @@
+(define str "This is a test string")
+(print str)
+
+(print "-----------------")
+
 (define square(lambda (x) (* x x)))
-
 (define x 5)
-
 (print "squared: " (square x))
 
-(if (> 1 0) (print "1 > 0 evalution is:" true) (print "1 < 0 evaluation is:" false))
-(if (< 1 0) (print "1 > 0 evalution is:" true) (print "1 < 0 evaluation is:" false))
+(print "-----------------")
+
+(if (> 1 0) (print "1 > 0 evalution is:" true) (print "1 > 0 evaluation is:" false))
+(if (< 1 0) (print "1 < 0 evalution is:" true) (print "1 < 0 evaluation is:" false))
+(if (<= 1 0) (print "1 <= 0 evalution is:" true) (print "1 <= 0 evaluation is:" false))
+(if (>= 1 0) (print "1 >= 0 evalution is:" true) (print "1 >= 0 evaluation is:" false))
+(if (!= 1 0) (print "1 != 0 evalution is:" true) (print "1 != 0 evaluation is:" false))
+(if (== 1 0) (print "1 == 0 evalution is:" true) (print "1 == 0 evaluation is:" false))
+
+(print "-----------------")
 
 (define ternary1 (if (== x 5) ("yes") ("no")))
 (define ternary2 (if (!= x 5) ("yes") ("no")))
+(print "ternary 1 says" ternary1)
+(print "ternary 2 says" ternary2)
 
-(print "ternary 1" ternary1)
-(print "ternary 2" ternary2)
-
-(define str "test string")
-(print str)
+(print "-----------------")
 
 (redefine str "that's all folks!")
 (print str)
